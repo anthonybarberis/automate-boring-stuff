1d67cc27121197449eea87cf4f54fa806e410ccb
Anthony Barberis <anthonybarberis@outlook.com>
2020-08-02 16:31:18 -0700

    Created guessNumber()

--- intro.py
+++ intro.py
@@ -6,6 +6,24 @@ def askName(owner):
         return f'{name}, what are you doing on {owner}\'s computer?'
 
 
+def guessNumber():
+    import random
+    randNum = random.randint(1, 20)
+    print('I am thinking of a number between 1 and 20')
+    guess = None
+    guesses = 0
+    while guess != randNum:
+        guess = int(input('Guess my numnber: '))
+        guesses += 1
+        if guess < randNum:
+            print('Too low')
+        elif guess > randNum:
+            print('Too high')
+        elif guess == randNum:
+            print(f'You got it in {guesses} guesses!')
+
+
 if __name__ == '__main__':
     # print(askName('Anthony'))
+    # guessNumber()
     pass
