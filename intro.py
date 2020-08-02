f745cbece7686f87cfaf36417453ad87d8b1d33a
Anthony Barberis <anthonybarberis@outlook.com>
2020-08-02 13:47:23 -0700

    Removed side effects from askName

--- intro.py
+++ intro.py
@@ -2,11 +2,11 @@ def askName(owner):
     name = input('What is your name?')
 
     if name == owner:
-        print('Hello, ' + name)
+        return 'Hello, ' + name
     else:
-        print(f'{name}, what are you doing on {owner}\'s computer?')
+        return f'{name}, what are you doing on {owner}\'s computer?'
 
 
 if __name__ == '__main__':
-    #askName('Anthony')
+    #print(askName('Anthony'))
     pass
